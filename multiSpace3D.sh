d35ea2be8a560fa6e67d15bd033b8660ae73a2ae
dblanchemain <57360484+dblanchemain@users.noreply.github.com>
2021-01-15 09:50:53 +0100

    Update multiSpace3D.sh

--- multiSpace3D.sh
+++ multiSpace3D.sh
@@ -1,21 +1,21 @@
 #! /bin/sh 
 
-dir=$HOME/space3D
+dir=$HOME/multiSpace3D
 
-if [ -d $HOME/space3D ] ; then 
+if [ -d $HOME/multiSpace3D ] ; then 
 
-	echo "Le dossier de travail ($HOME)/space3D existe" 
+	echo "Le dossier de travail ($HOME)/multiSpace3D existe" 
 
 else 
 
-	mkdir $HOME/space3D 
+	mkdir $HOME/multiSpace3D 
 
-echo "Le dossier de travail ($HOME)/space3D est créé" 
+echo "Le dossier de travail ($HOME)/multiSpace3D est créé" 
 
-	cp  /usr/local/share/space3D/preferences.spc $HOME/space3D
-	cp -Rf /usr/local/share/space3D/Plugins/ $HOME/space3D
-	cp -Rf /usr/local/share/space3D/DSP/ $HOME/space3D/
+	cp  /usr/local/share/multiSpace3D/preferences.spc $HOME/multiSpace3D
+	cp -Rf /usr/local/share/multiSpace3D/Plugins/ $HOME/multiSpace3D
+	cp -Rf /usr/local/share/multiSpace3D/DSP/ $HOME/multiSpace3D/
 fi 
 
-/usr/local/bin/multiSpace3D.bin  /usr/local/share/space3D $HOME
+/usr/local/bin/multiSpace3D.bin  /usr/local/share/multiSpace3D $HOME
 
